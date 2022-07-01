ad1e29cf25679d268566d573e9751f164c63b4fc
xpole <newyeardreamer@gmail.com>
2022-07-01 11:44:47 +0300

    OSI2

--- src/main/java/SocketApp.java
+++ src/main/java/SocketApp.java
@@ -6,17 +6,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketApp {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         int port = 8080;
         try (ServerSocket serverSocket = new ServerSocket(port);
              Socket socket = serverSocket.accept();
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-             String resp = in.readLine();
-             out.println(String.format("Hi, %s ", resp));
+             System.out.println("New connection accepted");
+             final String name = in.readLine();
+             out.println(String.format("Hi, %s, your port is %d\" ", name, socket.getPort()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        }
     }
+}
 
